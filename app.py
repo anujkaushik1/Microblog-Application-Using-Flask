f9e8f61808ad4d02721390f571c51f6e26228016
Anuj Kaushik <anujkaushik1512@gmail.com>
2021-11-30 12:10:26 +0530

    Diplayed Past Enteries in Microblog

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import datetime
  
 app = Flask(__name__)
 
+entries = []
+
 @app.route("/", methods = ["GET","POST"])
 def home():
     if request.method == "POST":
@@ -10,7 +12,7 @@ def home():
 
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
 
-        print(entry_content, formatted_date)
+        entries.append((entry_content, formatted_date))
 
 
-    return render_template("home.html")
+    return render_template("home.html", entries = entries)
