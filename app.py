10e6b0c0af746b8b5ac5320fb140e8147cfa64df
Anuj Kaushik <anujkaushik1512@gmail.com>
2021-11-30 15:49:27 +0530

    Implemented MongoDB with Flask

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request
 import datetime
+from pymongo import MongoClient
  
 app = Flask(__name__)
+
+client = MongoClient("mongodb+srv://Anuj:Anuj123@cluster0.myrej.mongodb.net/test")
+
+app.db = client.Microblog    
 
 entries = []
 
@@ -14,16 +19,18 @@ def home():
 
         entries.append((entry_content, formatted_date))
 
-        enteries_with_date = [
+        app.db.entries.insert_one({"content" : entry_content, "date" : formatted_date})
+
+    enteries_with_date = [
             (
-                entry[0],
-                entry[1],
-                datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
 
             )
 
-            for entry in entries
+            for entry in app.db.entries.find({})
         ]
-
+        
 
     return render_template("home.html", entries = enteries_with_date)
